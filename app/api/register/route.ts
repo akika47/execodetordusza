cbca0c06c4252b670148a5671e19b18bca6da496
akika47 <akika02531@gmail.com>
2024-11-09 18:42:12 +0100

    api tested

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -2,41 +2,44 @@ import pool from '@/app/libs/mysql';
 import { NextRequest, NextResponse } from 'next/server';
 
 type User = {
-    username: string
-    password: string
-    email: string
-    phoneNumber: string
-    role: string
-    schoolId: number
-}
+  username: string;
+  password: string;
+  email: string;
+  phoneNumber: string;
+  role: string;
+  schoolId: number;
+};
 
 export async function POST(req: NextRequest) {
-    try {
-        const { username, password, email, phoneNumber, role, schoolId } = await req.json() as User;
+  try {
+    const { username, password, email, phoneNumber, role, schoolId } = (await req.json()) as User;
+    console.log('Received data:', { username, password, email, phoneNumber, role, schoolId });
 
-        const db = await pool.getConnection()
-        const query = 'insert into users (username, password, email, phoneNumber, role, schoolId) values (?, ?, ?, ?, ?, ?)'
-        const [result] = await db.execute(query, [username, password, email, phoneNumber, role, schoolId]);
-        db.release()
-        
-        return NextResponse.json({
-            status: 200,
-            data: [
-                {
-                    "id": (result as any).insertId,
-                    "username": username,
-                    "password": password,
-                    "email": email,
-                    "phoneNumber": phoneNumber,
-                    "role": role,
-                    "schoolId": schoolId
-                }
-            ]
-        })
-    } catch (error) {
-        return NextResponse.json({
-            status: 404,
-            data: `Hiba történt: ${error}`
-        })
-    }
+    const db = await pool.getConnection();
+    console.log('Database connection established');
+
+    const query = 'INSERT INTO users (username, password, email, phoneNumber, role, schoolId) VALUES (?, ?, ?, ?, ?, ?)';
+    const [result] = await db.execute(query, [username, password, email, phoneNumber, role, schoolId]);
+    console.log('Query executed, result:', result);
+    db.release();
+
+    return NextResponse.json({
+      status: 200,
+      data: {
+        id: (result as any).insertId,
+        username,
+        email,
+        phoneNumber,
+        role,
+        schoolId,
+      },
+    });
+  } catch (error) {
+    console.error('Error during user registration:', error);
+
+    return NextResponse.json({
+      status: 500,
+      data: `An error occurred: ${error}`,
+    });
+  }
 }
