f54a9b83a723947d4030c3d11cd1d40b51f7d1f2
marcellnagy05 <marcika863@gmail.com>
2024-11-09 17:11:29 +0100

    initial

---
+++ app/About/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "@/app/layout";
+import Header from "@/app/components/Header";
+
+export default function About() {
+  return (
+    <>
+      <Header />
+      <Layout>
+        <div>About Us</div>
+      </Layout>
+    </>
+  );
+}
---
+++ app/News/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "@/app/layout";
+import Header from "@/app/components/Header";
+
+export default function News() {
+  return (
+    <>
+      <Header />
+      <Layout>
+        <div>News</div>
+      </Layout>
+    </>
+  );
+}
